ecfecd08a4415d07c16224fa4a3d7f3898fdd283
Sylvain <sylvain.lapeyrade@hotmail.fr>
2019-09-23 09:58:30 +0200

    initial commit

---
+++ title_screen/title_screen.gd
@@ -0,0 +1,30 @@
+extends Control
+
+var scene_path_to_load
+
+func _ready():
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
+	for button in $Menu/CenterRow/Buttons.get_children():
+		if button != $Menu/CenterRow/Buttons/Exit:
+			button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+func _physics_process(delta):
+	for button in $Menu/CenterRow/Buttons.get_children():
+		if button.is_hovered() == true:
+			button.grab_focus()
+
+func _on_Button_pressed(scene_to_load):
+	$FadeIn.show()
+	$FadeIn.fade_in()
+	scene_path_to_load = scene_to_load
+
+
+func _on_FadeIn_fade_finished():
+	$FadeIn.hide()
+	get_tree().change_scene(scene_path_to_load)
+
+
+func _on_Exit_pressed():
+	$FadeIn.show()
+	$FadeIn.fade_in()
+	get_tree().quit()
